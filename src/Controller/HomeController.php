7a541b666cfb5792292d2cf2ffce0216741cd27d
despotovic99 <d.neca99@gmail.com>
2022-04-01 15:40:32 +0200

    safety

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FrontController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'controller_name' => 'HomeController',
         ]);
     }
 }
